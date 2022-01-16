6402f7e8055fa6cee43fc65abfc43dbe2888e699
chandreshpokiya <chandreshpokiya6574@gmail.com>
2022-01-16 18:19:02 +0530

    Footer Added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ const menuBtn = document.querySelector(".menu-icon");
 const clsBtn = document.querySelector(".cls-btn");
 const menu = document.querySelector(".hamburger");
 
+var scrollButton = document.querySelector(".scrollTop");
+
+if (scrollButton) {
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+}
+
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.add("d-none");
   menu.classList.remove("d-none");
